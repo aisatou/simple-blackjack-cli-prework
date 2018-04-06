c1d212a920273ce34c5ed8d042c741103428d68b
Aisatou Diallo <diallo.aisatou@gmail.com>
2018-04-06 03:09:12 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -48,10 +48,9 @@ def runner
   welcome
   card_total = initial_round
   until card_total > 21
-  hit?(card_total)
-  break
-  display_card_total(card_total)
-end
-end_game(card_total)
+  card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
 end
     
